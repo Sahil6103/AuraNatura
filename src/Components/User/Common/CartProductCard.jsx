f4b4da4701fa0fcaa4e351307081a3c15e6743d3
sahil <sahildhandhukiya0@gmail.com>
2024-10-10 11:40:31 +0530

    10-Oct

---
+++ src/Components/User/Common/CartProductCard.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { MenPerfume } from "../../../assets/index";
+import { PlusSignIcon, Remove01Icon, Delete02Icon } from "hugeicons-react";
+
+export const CartProductCard = () => {
+  return (
+    <>
+      <div className="cart w-full lg:w-[85%]">
+        <div className="product-card py-2 flex justify-evenly items-center shadow-[0px_1px_0px_rgba(17,17,26,0.05),_0px_0px_8px_rgba(17,17,26,0.1)] ">
+          <div className="product-img">
+            <img src={MenPerfume} alt="" className="w-[150px]" />
+          </div>
+          <div className="prodcut-details md:w-[60%] flex flex-col gap-1.5 md:gap-3 px-2 md:px-4 py-2">
+            <h1 className="text-[1.1rem] md:text-[1.3rem] leading-[25px] font-semibold">
+              Lorem ipsum dolor sit amet
+            </h1>
+            <span className="text-[1.1rem] md:text-[1.2rem]">Size : 50ML</span>
+            <div className="qty">
+              <div className="buttons flex">
+                <button className="px-3 py-1 bg-black text-white text-center text-[1.2rem]">
+                  <PlusSignIcon color="#fff" size={18} />
+                </button>
+                <input
+                  type="text"
+                  disabled
+                  className="py-1 w-[50px] text-center border border-gray-400"
+                  value={1}
+                />
+                <button className="px-3 py-1 bg-black text-white text-center text-[1.2rem]">
+                  <Remove01Icon color="#fff" size={18} />
+                </button>
+              </div>
+            </div>
+            <span className="text-[1.2rem] md:text-[1.5rem] font-bold">
+              &#8377;500.00
+            </span>
+            <div className="delete top-5 right-5 cursor-pointer mb-2 flex items-center gap-2 w-fit">
+              <Delete02Icon
+                color="black"
+                size="22"
+                className="md:w-[24px] md:h-[24px]"
+              />
+              <span className="text-[1.1rem]">Remove</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
--- src/Components/User/Pages/Cart.jsx
+++ src/Components/User/Pages/Cart.jsx
@@ -1,5 +1,61 @@
 import React from "react";
+import { HeadingBanner } from "../Common/HeadingBanner";
+import { CartProductCard } from "../Common/CartProductCard";
+import { Link } from "react-router-dom";
 
 export const Cart = () => {
-  return <div>Cart</div>;
+  return (
+    <>
+      <HeadingBanner heading="Shopping Cart" page="Shopping Cart" />
+      <div className="main-cart px-4 md:px-10 lg:px-16 py-10 flex flex-col md:flex-row gap-8 relative">
+        <div className="product-card flex flex-col gap-8 w-full lg:w-[70%] flex-1">
+          <CartProductCard />
+          <CartProductCard />
+          <CartProductCard />
+        </div>
+        <div className="product-order xl:w-[30%] px-4 py-2">
+          <div className="order flex flex-col gap-2">
+            <h1 className="text-[1.5rem] font-bold">Order Summary</h1>
+            <div className="total-item flex justify-between items-center">
+              <span>Total Items</span>
+              <span>3</span>
+            </div>
+            <div className="Subtotal flex justify-between items-center">
+              <span className="text-[1.1rem]">Sub Total</span>
+              <span className="text-[1.1rem]">&#8377;3000.00</span>
+            </div>
+            <div className="delivery flex justify-between items-center">
+              <span className="text-[1.1rem]">Delivary Charge</span>
+              <span className="text-[1.1rem]">&#8377;00.00</span>
+            </div>
+            <hr className=" border border-gray-300" />
+            <div className="total flex justify-between items-center">
+              <span className="text-[1.1rem] font-semibold">Total</span>
+              <span className="text-[1.1rem] font-semibold">
+                &#8377;3000.00
+              </span>
+            </div>
+            <Link
+              to="/checkout"
+              className="bg-[#b48b5e] text-[#f0efed] border-2 border-[#b48b5e] text-center mt-5 px-12 p-1.5 text-[1.3rem] hidden md:flex justify-center items-center"
+            >
+              Place Order
+            </Link>
+          </div>
+        </div>
+        <div className="small-device-total bg-[#f0efed] w-full md:hidden flex justify-between sticky bottom-0 py-2">
+          <div className="total-price flex flex-col">
+            <span className="text-[1rem]">Total</span>
+            <span className="text-[1.1rem] font-bold">&#8377;3000.00</span>
+          </div>
+          <Link
+            to="/checkout"
+            className="flex justify-center items-center text-center bg-[#b48b5e] text-[#f0efed] px-5"
+          >
+            Place Order
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 };
