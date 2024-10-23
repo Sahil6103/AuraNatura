ab3a72beeeedb86f3e79391d46a7e5d4bcc9ba6e
sahil <sahildhandhukiya0@gmail.com>
2024-10-23 14:38:00 +0530

    22-Oct

---
+++ src/Components/User/Common/Preloader.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { FullLogo } from "../../../assets/index";
+
+export const Preloader = () => {
+  return (
+    <>
+      <div className="preloader h-screen flex justify-center items-center bg-[#f0efed]">
+        <div className="main-loader flex flex-col justify-center items-center gap-8">
+          <div className="logo w-[70%] flex justify-center items-center">
+            <img src={FullLogo} alt="" className="w-full" />
+          </div>
+          <div className="loader flex flex-col justify-center items-center gap-2">
+            <div className="dots1 flex justify-center items-center gap-2">
+              <div className="dot w-[10px] md:w-[15px] h-[10px] md:h-[15px] bg-[#b48b5e]"></div>
+              <div className="dot w-[10px] md:w-[15px] h-[10px] md:h-[15px] bg-[#b48b5e]"></div>
+            </div>
+            <div className="dots2 flex justify-center items-center gap-2">
+              <div className="dot w-[10px] md:w-[15px] h-[10px] md:h-[15px] bg-[#b48b5e]"></div>
+              <div className="dot w-[10px] md:w-[15px] h-[10px] md:h-[15px] bg-[#b48b5e]"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
--- src/Layout/UserLayout.jsx
+++ src/Layout/UserLayout.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import { Header } from "../Components/User/Header";
 import { Footer } from "../Components/User/Footer";
+import { Preloader } from "../Components/User/Common/Preloader";
 
 export const UserLayout = () => {
-  return (
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000); // Preloader duration
+
+    return () => clearTimeout(timer); // Cleanup timer on unmount
+  }, []);
+
+  return loading ? (
+    <Preloader />
+  ) : (
     <div className="user-layout">
       <Header />
       <Outlet />
